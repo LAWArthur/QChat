7441a1e45e3272cca0ab4c35e29dc0da1ec9659a
LAWArthur <jincai3335_micro@163.com>
2018-07-01 16:06:02 +0800

    Add files

---
+++ bin/Debug/AppX/ep1/qchatcharacters.js
@@ -0,0 +1,16 @@
+﻿var characters = [
+    {
+        name: "demo",
+        index: 0,
+        actions: [msg(0, "在嘛"), mymsg(0, "在"), msg(0, "找你聊天"), choice(0, "好", "不好"), choice_based(0, msg(0, "好啊"), msg(0,"好吧。。。。。。")), opn(1), end(0)],
+        latest: "",
+        chatEnabled: false
+    },
+    {
+        name: "XXX",
+        index: 0,
+        actions: [end(1),msg(1,"HAHA"),end(1)],
+        latest: "",
+        chatEnabled: false
+    }
+]
---
+++ bin/Debug/AppX/js/qchatapis.js
@@ -0,0 +1,73 @@
+﻿function msg(ref, ms) {
+    return function () {
+        t = setInterval(function () {
+            $("#" + ref).append($("<li></li>").append($("<img/>").attr("src", "../images/" + characters[ref].name + ".png")).append(ms));
+            clearInterval(t);
+            characters[ref].actions[++characters[ref].index]();
+        }, 500 * ms.length);
+    };
+}
+
+function mymsg(ref, ms) {
+    return function () {
+        repeats = 0;
+        var t = setInterval(function () {
+            if (repeats == ms.length) {
+                $(".chatbox span").empty();
+                $("#" + ref).append($("<li></li>").append($("<div/>").css({ "width": "100%", "text-align": "right" }).append(ms).append($("<img/>").attr("src", "../images/me.png"))));
+                clearInterval(t);
+                characters[ref].actions[++characters[ref].index]();
+            } else {
+                $(".chatbox span").append(ms[repeats]);
+                repeats++;
+            }
+        }, 500);
+    };
+}
+
+function choice(ref, a, b, c = null, d = null) {
+    return function () {
+        $(".chatbox div").append($("<button/>").text(a).width(100).height(25).click(function () { $(".chatbox div").empty(); choices.push(0); mymsg(ref, a)(); })).append($("<button/>").text(b).width(100).height(25).click(function () { $(".chatbox div").empty(); choices.push(1); mymsg(ref, b)(); }));
+        if (c != null) {
+            $(".chatbox div").append($("<button/>").text(c).width(100).height(25).click(function () { $(".chatbox div").empty(); choices.push(2); mymsg(ref, c)(); }))
+        }
+        if (d != null) {
+            $(".chatbox div").append($("<button/>").text(d).width(100).height(25).click(function () { $(".chatbox div").empty(); choices.push(3); mymsg(ref, d)(); }))
+        }
+    }
+}
+
+function choice_based(index, a, b, c = null, d = null) {
+    return function () {
+        switch (choices[index]) {
+            case 0:
+                a();
+                break;
+            case 1:
+                b();
+                break;
+            case 2:
+                c();
+                break;
+            case 3:
+                d();
+                break;
+        }
+    }
+}
+
+function opn(ref) {
+    return function () { characters[ref].index++ };
+}
+
+function end(ref) {
+    return function () {
+        return;
+    }
+}
+
+var choices = [];
+
+var repeats = 0;
+
+var t;
